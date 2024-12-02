f2b23e434b66bb5267afa19cffec7b01ccae0e58
DearrLo <viellorette@gmail.com>
2024-12-02 13:56:32 +0100

    fixing pictures attached to the messages 1

--- Dials/salutations.js
+++ Dials/salutations.js
@@ -1,24 +1,43 @@
+const { AttachmentBuilder } = require('discord.js');
+const path = require('path');
 
- 
 // salutations responses
 const salutations = [
-  'Hello to you too, {username}. How are you doing? https://64.media.tumblr.com/f392c89cbdce8acad50e4fa0790cbb49/tumblr_inline_pr3szbEuWA1vhssku_250.gif',
-  '*Nods* {username}. It\'s good to see you too, pal. https://64.media.tumblr.com/9701af6ab4e2ba4b197145a0b0addf9e/tumblr_inline_p8ozuaF09F1swygin_250.gifv',
-  'Morning {username}! You didn\'t see Thor? https://i.makeagif.com/media/1-12-2017/PsShE9.gif',
-  'Hi {username}! You didn\'t see Tony? https://64.media.tumblr.com/8287de7011ad9e1015e23903af2e4324/dc65c6cb35aadec2-9a/s540x810/cb6e7144d352a70ddaf8dcfd4bc23550816123c8.gifv',
-  'Hi {username}! You didn\'t see Clint? https://i.pinimg.com/originals/a3/4d/e1/a34de1361f265b72de7e9a1039d9454c.gif',
-  'Hey {username}! You didn\'t see my peanut butter sandwich? https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHd0ZWxuajYxeW5senFnY3J4bm1idm15bWV2MHY4eG1sNTQ0amRqMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VKFfxB2skRYZO/giphy.webp',
-  'Hey, {username}! Hope you\'re doing just fine, dear. https://i.pinimg.com/originals/a3/4d/e1/a34de1361f265b72de7e9a1039d9454c.gif',
-  'Hey {username}. https://64.media.tumblr.com/8287de7011ad9e1015e23903af2e4324/dc65c6cb35aadec2-9a/s540x810/cb6e7144d352a70ddaf8dcfd4bc23550816123c8.gifv'
+  { text: 'Hello to you too, {username}. How are you doing?', image: 'nat1.jpg' },
+  { text: '*Nods* {username}. It\'s good to see you too, pal.', image: 'nat1.jpg' },
+  { text: 'Morning {username}! You didn\'t see Thor?', image: 'nat1.jpg' },
+  { text: 'Hi {username}! You didn\'t see Tony?', image: 'nat2.jpg' },
+  { text: 'Hi {username}! You didn\'t see Clint?', image: 'nat2.jpg' },
+  { text: 'Hey {username}! You didn\'t see my peanut butter sandwich?', image: 'nat3.jpg' },
+  { text: 'Hey, {username}! Hope you\'re doing just fine, dear.', image: 'nat3.jpg' },
+  { text: 'Hey {username}.', image: 'nat3.jpg' }
 ];
 
 function handleSalutations(message) {
-    const userMessage = message.content.toLowerCase();
-    if (userMessage.includes('bonjour') || userMessage.includes('hello') || userMessage.includes('hi') || userMessage.includes('yo') || userMessage.includes('hey')) {
-      let randomSalutation = salutations[Math.floor(Math.random() * salutations.length)];
-      randomSalutation = randomSalutation.replace('{username}', message.author.username);
-      message.reply(randomSalutation);
+  const userMessage = message.content.toLowerCase();
+
+  if (userMessage.includes('bonjour') || userMessage.includes('hello') || userMessage.includes('hi') || userMessage.includes('yo') || userMessage.includes('hey')) {
+    try {
+      // Sélectionne une salutation aléatoire
+      const randomSalutation = salutations[Math.floor(Math.random() * salutations.length)];
+      const textWithUsername = randomSalutation.text.replace('{username}', message.author.username);
+
+      // Crée un chemin vers l'image en local
+      const imagePath = path.join(__dirname, '../Images/relatedtoSalutations', randomSalutation.image);
+      console.log("Image Path:", imagePath); // Debugging log pour vérifier le chemin
+
+      // Crée un attachment avec l'image
+      const attachment = new AttachmentBuilder(imagePath);
+
+      // Répond au message avec le texte et l'image attachée
+      message.reply({ content: textWithUsername, files: [attachment] });
+      
+    } catch (error) {
+      // Gestion des erreurs
+      console.error('Error sending message with attachment:', error);
+      message.reply('Sorry, something went wrong when trying to send an image.');
     }
   }
+}
   
   module.exports = { handleSalutations };
